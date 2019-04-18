b42c9f109d8ea621fa08a2267713f8f66bc7c173
Arushi03 <38400212+Arushi03@users.noreply.github.com>
2019-04-18 22:12:35 +0530

    Add files via upload

--- index.blade.php
+++ index.blade.php
@@ -1,6 +1,31 @@
-@foreach($users as $user)
-	<li>{!! $user['name'] !!} age:{!! $user['age'] !!} from {!! $user['place'] !!}
-	</li>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
 
-	{{ $user['name'] }}
-@endforeach
+	@include('hospitall.client.layouts.nav')
+
+	<title>Home Page</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css')}}">
+
+</head>
+<body style="background-image: url('http://www.munsonhealthcare.org/upload/MediaGallery/Albums/101/Items/28231HomepageGraphics_CAD_ER.jpg'); background-repeat: no-repeat; background-size: cover;">
+<div class="container">
+	@if(session('success'))
+		<div class="alert alert-warning">
+
+			{{session('success')}}
+		</div>
+	@endif
+	<br>
+<div align="center">
+ <h1 style="font-family: 'Roboto Slab', serif;
+color: midnightblue ; " ><b>A MEDICAL ASSISTANT FOR YOU</b></h1>
+ <p style="font-size: 20px;" class=""><b>Fast. Friendly. Accurate. <br>For all patients.</b></p>
+ </div>
+ <div class="container">
+ @include('hospitall.client.layouts.timetable');
+  
+</div>
+</body>
+</html>
